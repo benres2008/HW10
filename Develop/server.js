3f56034ddf0771d9367c0e812ef04c40537e2470
rez <benjaminresnicoff@icloud.com>
2020-10-24 19:22:32 -0500

    tried to finish homework to no avail

--- Develop/server.js
+++ Develop/server.js
@@ -1,34 +1,75 @@
 const express = require("express");
+const bodyParser = require("body-parser")
 const path= require("path");
-const; 
-const { fstat } = require("fs");
-
-
+const fs = require("fs");
 const app = express();
 const PORT = 3000;
 
+var rootObj = {root: __dirname + "/public"}
+
+app.use(bodyParser.urlencoded({extended: false }))
+
+app.use(express.static(path.join(__dirname, "/public")))
+
 app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname,"./public/index.html"))
+    res.sendFile("index.html", rootObj)
 })
 
 app.get("/notes", function (req, res){
-    res.sendFile(path.join(__dirname, "./public/notes.html"))
+    res.sendFile("/notes.html", rootObj)
 })
 
 
 app.get("/api/notes", function (req, res) {
-    fs.readFile("db/db.json", "utf8", function(err, data){
-        res.json(data)
-    })
+    console.log("/api/notesget")
+    let json = getJson()
+    console.log(json)
+    res.json(json)
+})
     
-})
 app.post("/api/notes/:id", function (req, res) {
-    res.json()
+    console.log("/api/notes/:iddelete")
+    deleteNoteFromJSON(req.params.id)
+    res.json(getJson())
 })
-app.delete("/api/notes", function (req, res) {
-    res.json()
+app.delete("/api/notes/:id", function (req, res) {
+    console.log("/api/notes/:iddelete")
+    deleteNoteFromJSON(req.params.id)
+    res.json(getJson())
+
 })
 
 app.listen(PORT, function(){
     console.log("Server is listening on PORT: 3000")
 })
+
+function getJson(){
+    let data = fs.readFileSync(__dirname + "/db/db.json")
+    let json = JSON.parse(data)
+    return json
+}
+
+function createNoteObject(data){
+    let obj = {
+        title: data.title,
+        text: data.text,
+        complete: false,
+        hidden: false}
+    return obj
+}
+function addNoteToJSON(note){
+    let json = getJson()
+    let newNote = createNoteObject(note)
+    json.push(newNote)
+    saveJSON(json)
+}
+
+function saveJSON(jsonData){
+    let data = JSON.stringify(jsonData)
+    fs.writeFileSync(__dirname + "/db/db.json", data)
+}
+function deleteNoteFromJSON(id){
+    let json = getJson()
+    json[id].hide = true
+    saveJSON(json)
+}
